51389c332c6afffe74aff6b679e35ff83e8378b7
JoshuaW1990 <jun.wang0226@hotmail.com>
2020-09-13 22:56:44 +0800

    update

--- Week_01/trap.py
+++ Week_01/trap.py
@@ -1,4 +1,25 @@
 class Solution:
     # def trap(self, height: List[int]) -> int:
     def trap(self, height):
-        pass
+        if len(height) < 3:
+            return 0
+        result = 0
+        stack = [0]
+        index = 1
+        while index < len(height):
+            h = height[index]
+            while len(stack) > 0 and h > height[stack[-1]]:
+                topIndex = stack.pop()
+                if len(stack) <= 0:
+                    break
+                minHeight = min(h, height[stack[-1]])
+                distance = index - stack[-1] - 1
+                volume = distance * (minHeight - height[topIndex])
+                result += volume
+            stack.append(index)
+            index += 1
+        return result
+
+solution = Solution()
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+print(solution.trap(height))
